3949f7c0aa44c66419e07755f9160f6b9eed6744
cho seonghoon <chsida655@gmail.com>
2022-10-02 23:28:35 +0900

    Refactoring: delete home page

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,38 +1,12 @@
 import { NextPage } from "next";
-import React, { useEffect, useState } from "react";
-import FileContainer from "../../components/sub/FileContainer";
-import { v4 as uuidv4 } from "uuid";
-import io from "socket.io-client";
+import React from "react";
 import styled from "@emotion/styled";
 
-const HeartIcon = styled.div`
-  width: 20px;
-  height: 20px;
-  background: #ea2027;
-  position: relative;
-  transform: rotate(45deg);
-
-  &:before,
-  &:after {
-    content: "";
-    width: 20px;
-    height: 20px;
-    position: absolute;
-    border-radius: 50%;
-    background: #ea2027;
-  }
-  &:before {
-    left: -50%;
-  }
-  &:after {
-    top: -50%;
-  }
-`;
 
 const Home: NextPage = () => {
   return (
     <div>
-      <HeartIcon />
+    
     </div>
   );
 };
