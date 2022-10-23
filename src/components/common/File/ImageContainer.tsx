6fb62ba25882cf615cf2775b11a270b4cae5915a
cho seonghoon <chsida655@gmail.com>
2022-10-23 23:58:27 +0900

    Refactoring: type 예외 설정

--- src/components/common/File/ImageContainer.tsx
+++ src/components/common/File/ImageContainer.tsx
@@ -7,15 +7,13 @@ interface FileProps {
 }
 
 const ImageContainer: React.FC<FileProps> = (props) => {
-  console.log(props.size);
-
   return (
     <div>
       <Image
         src={props.url}
         alt={props.fileName}
-        width={props.size.length > 0 ? props.size[0] : 300}
-        height={props.size.length > 0 ? props.size[1] : 300}
+        width={props.size ? props.size[0] : 300}
+        height={props.size ? props.size[1] : 300}
       />
     </div>
   );
