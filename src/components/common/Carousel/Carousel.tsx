8a248a535b911fa83bbf99f8e5b9e6c1f377b178
cho seonghoon <chsida655@gmail.com>
2022-10-23 23:57:59 +0900

    Refactoring: Carousel

--- src/components/common/Carousel/Carousel.tsx
+++ src/components/common/Carousel/Carousel.tsx
@@ -6,6 +6,7 @@ import styled from "@emotion/styled";
 
 import LeftArrow from "../../public/componentSVG/Arrow/Left_Arrow.svg";
 import RightArrow from "../../public/componentSVG/Arrow/Right_Arrow.svg";
+import { CarouselSetting } from "./CarouselSetting";
 
 const ArrowDiv = styled.div<{ possible: boolean }>`
   cursor: ${(props) => (props.possible ? "pointer" : "arrow")};
@@ -14,7 +15,10 @@ const ArrowDiv = styled.div<{ possible: boolean }>`
 const NextArrow = (props) => {
   const { onClick } = props;
   return (
-    <ArrowDiv onClick={props.possible ? onClick : null} possible={props.possible}>
+    <ArrowDiv
+      onClick={props.possible ? onClick : null}
+      possible={props.possible}
+    >
       <RightArrow fill={props.possible ? "#47d2d2" : "#aaaaaa"} />
     </ArrowDiv>
   );
@@ -23,7 +27,10 @@ const NextArrow = (props) => {
 const PrevArrow = (props) => {
   const { onClick } = props;
   return (
-    <ArrowDiv onClick={props.possible ? onClick : null} possible={props.possible}>
+    <ArrowDiv
+      onClick={props.possible ? onClick : null}
+      possible={props.possible}
+    >
       <LeftArrow fill={props.possible ? "#47d2d2" : "#aaaaaa"} />
     </ArrowDiv>
   );
@@ -60,49 +67,10 @@ export const SlideButton = ({ slideRef, select, setSelect, length }) => {
 };
 
 const Carousel = (props) => {
-  const settings = {
-    dots: false,
-    infinite: false,
-    lazyLoad: true,
-    focusOnSelect: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    initialSlide: 0,
-
-    responsive: [
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: false,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2,
-          infinite: false,
-        },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: false,
-        },
-      },
-    ],
-  };
-
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <CustomSlider>
-        <Slider ref={props.setSlideRef} {...settings}>
+        <Slider ref={props.setSlideRef} {...CarouselSetting}>
           {props.children}
         </Slider>
       </CustomSlider>
